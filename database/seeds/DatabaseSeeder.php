547e4468b5be8414e926122019c37535083a78dc
Fil <fil.elman@greyandgreentech.com>
2017-06-19 15:44:53 +0800

    configure and enabled UsersTableSeeder.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
     $this->call(DepartmentsTableSeeder::class);
     $this->call(PositionsTableSeeder::class);
     $this->call(OrganizationsTableSeeder::class);
+    $this->call(UsersTableSeeder::class);
   }
 }
