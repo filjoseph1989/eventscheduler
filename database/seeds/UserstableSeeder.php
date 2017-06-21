b74440a07c6beeb98a81e50e22062983e49d0252
jlvoice777 <fil@greyandgreentech.com>
2017-06-21 11:45:38 +0800

    Issue 5: Cannot seed user

--- database/seeds/UserstableSeeder.php
+++ database/seeds/UserstableSeeder.php
@@ -12,27 +12,25 @@ class UserstableSeeder extends Seeder
   public function run()
   {
     DB::table('users')->insert([
-      array(
-        'user_account_id'    => '1',
-        'course_id'          => '1',
-        'department_id'      => '2',
-        'position_id'        => '8',
-        'account_number'     => '1',
-        'email'              => 'liz@email.com',
-        'password'           => bcrypt('password'),
-        'first_name'         => 'Liz',
-        'last_name'          => 'Dee',
-        'middle_name'        => 'G',
-        'suffix_name'        => NULL,
-        'facebook_email'     => 'liz@facebook.com',
-        'twitter_username'   => 'liz@twitter.com',
-        'instagram_username' => 'liz@instagram.com',
-        'contact_number'     => '09128633866',
-        'status'             => '1',
-        'remember_token'     => NULL,
-        'created_at'         => date('Y-m-d H:i:s'),
-        'updated_at'         => date('Y-m-d H:i:s')
-      )
+        array(
+            'user_account_id'    => '1',
+            'course_id'          => '9',
+            'department_id'      => '2',
+            'position_id'        => '8',
+            'account_number'     => '1',
+            'email'              => 'user@email.com',
+            'password'           => bcrypt('password'),
+            'first_name'         => 'user',
+            'last_name'          => 'user',
+            'middle_name'        => 'i',
+            'suffix_name'        => 'i',
+            'facebook_username'  => 'user@facebook.com',
+            'twitter_username'   => 'user@twitter.com',
+            'instagram_username' => 'user@instagram.com',
+            'mobile_number'      => '4294967295',
+            'status'             => '1',
+            'remember_token'     => NULL
+        )
     ]);
   }
 }
