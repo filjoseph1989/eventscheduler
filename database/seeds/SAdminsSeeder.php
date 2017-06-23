5a4c91693866dde6a542a42c600d9cbdbaa627dc
jlvoice777 <fil@greyandgreentech.com>
2017-06-23 10:07:20 +0800

    Fixed the issue of admin seeder which is value are not match with the columns on tabls s_admins, and remove other data and remain only the admin

--- database/seeds/SAdminsSeeder.php
+++ database/seeds/SAdminsSeeder.php
@@ -14,23 +14,6 @@ class SAdminsSeeder extends Seeder
     {
         DB::table('s_admins')->insert([
             array(
-                'account_number'     => '2012-35943',
-                'email'              => 'liz@email.com',
-                'password'           => bcrypt('password'),
-                'first_name'         => 'Liz',
-                'last_name'          => 'de Guzman',
-                'middle_name'        => 'Nawa',
-                'suffix_name'        => 'A',
-                'facebook_username'  => 'admin@facebook',
-                'twitter_username'   => 'admin@twitter.com',
-                'instagram_username' => 'admin@instagram.com',
-                'mobile_number'      => '09121234567',
-                'mobile_number'      => '958633866',
-                'remember_token'     => NULL,
-                'created_at'         => date('Y-m-d H:i:s'),
-                'updated_at'         => date('Y-m-d H:i:s')
-            ),
-            array(
                 'account_number'     => '1',
                 'email'              => 'admin@email.com',
                 'password'           => bcrypt('password'),
@@ -38,15 +21,15 @@ class SAdminsSeeder extends Seeder
                 'last_name'          => 'Admin',
                 'middle_name'        => 'A',
                 'suffix_name'        => 'A',
-                'facebook_username'  => 'admin@facebook',
-                'twitter_username'   => 'admin@twitter.com',
-                'instagram_username' => 'admin@instagram.com',
-                'mobile_number'      => '09121234567',
-                'status'             => '1',
+                'facebook_username'  => NULL,
+                'twitter_username'   => NULL,
+                'instagram_username' => NULL,
+                'mobile_number'      => '',
+                'status'             => '0',
                 'remember_token'     => NULL,
                 'created_at'         => date('Y-m-d H:i:s'),
                 'updated_at'         => date('Y-m-d H:i:s')
-            )
+            ),
         ]);
     }
 }
