6ec3ec1ca3e3b11265046ff2bd4b8b11bce4002f
jlvoice777 <fil@greyandgreentech.com>
2017-06-23 10:05:35 +0800

    Include the admin seeder in DatabaseSeeder Class

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,19 +4,20 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-  /**
-   * Run the database seeds.
-   *
-   * @return void
-   */
-  public function run()
-  {
-    $this->call(EventCategoriesSeeder::class);
-    $this->call(CoursesTableSeeder::class);
-    $this->call(UserAccountsTableSeeder::class);
-    $this->call(DepartmentsTableSeeder::class);
-    $this->call(PositionsTableSeeder::class);
-    $this->call(OrganizationsTableSeeder::class);
-    $this->call(UserstableSeeder::class);
-  }
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(EventCategoriesSeeder::class);
+        $this->call(CoursesTableSeeder::class);
+        $this->call(UserAccountsTableSeeder::class);
+        $this->call(DepartmentsTableSeeder::class);
+        $this->call(PositionsTableSeeder::class);
+        $this->call(OrganizationsTableSeeder::class);
+        $this->call(UserstableSeeder::class);
+        $this->call(SAdminsSeeder::class);
+    }
 }
