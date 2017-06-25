0510cfac793f532b33ebb4332718a777e556b123
jlvoice777 <fil@greyandgreentech.com>
2017-06-25 09:47:02 +0800

    re-arrange the routing into more clean way

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Route::prefix('admin')->group(function() {
   Route::name('admin.user.register')->get('/register', 'AdminController@showRegisterForm');
   Route::name('admin.department.list')->get('/department/list', 'AdminController@showAllDepartmentList');
   Route::name('admin.position.list')->get('/position/list', 'AdminController@showAllPositionList');
-  Route::name('admin.organization.list')->get('/organization/list', 'AdminController@showAllOrganizationList');
+  Route::name('admin.user.organization.list')->get('/organization/list', 'AdminController@showAllOrganizationList');
   Route::name('admin.event.categories.list')->get('/event/categories/list', 'AdminController@showAllEvenCategoriesList');
   Route::name('admin.event.types.list')->get('/event/types/list', 'AdminController@showAllEventTypes');
   Route::name('admin.approvers.list')->get('/approvers/list', 'AdminController@showAllApprovers');
@@ -42,12 +42,17 @@ Route::prefix('admin')->group(function() {
 });
 /*
 |--------------------------------------------------------------------------
-| Organization routes
+| Auth Routes
 |--------------------------------------------------------------------------
 */
-Route::prefix('organization')->group(function() {
-  Route::name('organization.list')->get('/list', 'OrganizationController@index');
-  Route::name('organization.add')->get('/add', 'OrganizationController@create');
+Route::prefix('users')->group(function() {
+    Route::name('user.registered')->post('/registered', 'UsersController@create');
+    Route::name('user.account.register')->get('/register', 'UsersAccountAdminController@showRegisterForm');
+    Route::name('user.account.registered')->post('/registered', 'UsersAccountAdminController@create');
+    Route::name('user.course.register')->get('/course/register', 'CourseController@showRegisterForm');
+    Route::name('user.course.registered')->post('/course/registered', 'CourseController@create');
+    Route::name('user.organization.list')->get('/organization/list', 'OrganizationController@index');
+    Route::name('user.organization.add')->get('/organization/add', 'OrganizationController@create');
 });
 /*
 |--------------------------------------------------------------------------
@@ -56,23 +61,3 @@ Route::prefix('organization')->group(function() {
 */
 Route::name('notify.via.sms')->get('/notify_via_sms', 'smsNotifierController@index');
 Route::name('faceboo.notification')->get('/fb/post', 'HomeController@sendNotification');
-/*
-|--------------------------------------------------------------------------
-| Auth Routes
-|--------------------------------------------------------------------------
-*/
-Route::prefix('users')->group(function() {
-    Route::name('user.registered')->post('/registered', 'UsersController@create');
-});
-
-
-# ignore me for a moment
-Route::prefix('user-accounts')->group(function() {
-    Route::name('user-account.register')->get('/register', 'UsersAccountAdminController@showRegisterForm');
-    Route::name('user-account.registered')->post('/registered', 'UsersAccountAdminController@create');
-});
-
-Route::prefix('course')->group(function() {
-    Route::name('course.register')->get('/register', 'CourseController@showRegisterForm');
-    Route::name('course.registered')->post('/registered', 'CourseController@create');
-});
