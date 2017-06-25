9cb5f55ece59ebf62e2a9cf01fa06d5577b68c0e
jlvoice777 <fil@greyandgreentech.com>
2017-06-25 09:43:28 +0800

    Added fontawesome in master blade while making the admin.js optional as to when to load, depending on either before login or after login

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,10 +9,11 @@
   <link rel="icon" href="favicon.ico" type="image/x-icon">
   <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
+  <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet">
   <link href="{{asset('css/bootstrap.css')}}" rel="stylesheet">
   <link href="{{asset('css/waves.css')}}" rel="stylesheet" />
   <link href="{{asset('css/animate.css')}}" rel="stylesheet" />
-  <link href="{{asset('css/style.css')}}" rel="stylesheet">
+  <link href="{{asset('css/style.css')}}?v=0.2" rel="stylesheet">
 
   @yield('style')
 </head>
@@ -26,8 +27,12 @@
   <script src="{{asset('js/jquery.min.js')}}"></script>
   <script src="{{asset('js/bootstrap.js')}}"></script>
   <script src="{{asset('js/waves.js')}}"></script>
-  @yield('footer')
   <script src="{{asset('js/jquery.slimscroll.js')}}"></script>
-  <script src="{{asset('js/admin.js')}}"></script>
+
+  @if (Auth::check())
+    <script src="{{asset('js/admin.js')}}"></script>
+  @endif
+
+  @yield('footer')
 </body>
 </html>
