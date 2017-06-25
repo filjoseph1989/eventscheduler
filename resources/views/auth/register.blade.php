d099158155b394346e01c25f071254a062d36b15
jlvoice777 <fil@greyandgreentech.com>
2017-06-25 09:32:38 +0800

    Remove unused sidebar

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,6 +12,92 @@
       <a href="javascript:void(0);">Event<b>Scheduler</b></a>
     </div>
     <div class="card">
+        <div class="body">
+            <form id="sign_up" method="POST" action="{{ route('register') }}">
+                {{ csrf_field() }}
+                <div class="msg">Register a new membership</div>
+                <div class="input-group">
+                    <span class="input-group-addon">
+                        <i class="material-icons">person</i>
+                    </span>
+                    <div class="form-line">
+                        <input type="text" class="form-control" name="first_name" placeholder="Name" required autofocus>
+                        @if ($errors->has('first_name'))
+                          <span class="help-block"> <strong>{{ $errors->first('first_name') }}</strong> </span>
+                        @endif
+                    </div>
+                </div>
+                <div class="input-group">
+                    <span class="input-group-addon">
+                        <i class="material-icons">person</i>
+                    </span>
+                    <div class="form-line">
+                        <input type="text" class="form-control" name="last_name" placeholder="Last Name" required>
+                        @if ($errors->has('last_name'))
+                          <span class="help-block"> <strong>{{ $errors->first('last_name') }}</strong> </span>
+                        @endif
+                    </div>
+                </div>
+                <div class="input-group">
+                    <span class="input-group-addon">
+                        <i class="material-icons">person</i>
+                    </span>
+                    <div class="form-line">
+                        <input type="text" class="form-control" name="middle_name" placeholder="Middle Name" required>
+                        @if ($errors->has('middle_name'))
+                          <span class="help-block"> <strong>{{ $errors->first('middle_name') }}</strong> </span>
+                        @endif
+                    </div>
+                </div>
+                <div class="input-group">
+                    <span class="input-group-addon">
+                        <i class="material-icons">person</i>
+                    </span>
+                    <div class="form-line">
+                        <input type="text" class="form-control" name="suffix_name" placeholder="Suffix" required>
+                        @if ($errors->has('suffix_name'))
+                          <span class="help-block"> <strong>{{ $errors->first('suffix_name') }}</strong> </span>
+                        @endif
+                    </div>
+                </div>
+                <div class="input-group">
+                    <span class="input-group-addon">
+                        <i class="material-icons">email</i>
+                    </span>
+                    <div class="form-line">
+                        <input type="email" class="form-control" name="email" placeholder="Email Address" required>
+                    </div>
+                </div>
+                <div class="input-group">
+                    <span class="input-group-addon">
+                        <i class="material-icons">lock</i>
+                    </span>
+                    <div class="form-line">
+                        <input type="password" class="form-control" name="password" minlength="6" placeholder="Password" required>
+                    </div>
+                </div>
+                <div class="input-group">
+                    <span class="input-group-addon">
+                        <i class="material-icons">lock</i>
+                    </span>
+                    <div class="form-line">
+                        <input type="password" class="form-control" name="confirm" minlength="6" placeholder="Confirm Password" required>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" name="terms" id="terms" class="filled-in chk-col-pink">
+                    <label for="terms">I read and agree to the <a href="javascript:void(0);">terms of usage</a>.</label>
+                </div>
+
+                <button class="btn btn-block btn-lg bg-pink waves-effect" type="submit">SIGN UP</button>
+
+                <div class="m-t-25 m-b--5 align-center">
+                    <a href="sign-in.html">You already have a membership?</a>
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="card">
       <div class="body">
         <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
           {{ csrf_field() }}
