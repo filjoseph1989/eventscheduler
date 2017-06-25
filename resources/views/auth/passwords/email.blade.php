faa1ada5ebbdfb745330f0620a4f11cea40590e0
jlvoice777 <fil@greyandgreentech.com>
2017-06-25 14:41:11 +0800

    Improve the forgot password layout

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,46 +1,49 @@
 @extends('layouts.master')
 
+@section('style')
+  <link rel="stylesheet" href="{{ asset('css/waitMe.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/bootstrap-select.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/bootstrap-material-datetimepicker.css') }}">
+@endsection
+
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+  <div class="signup-box">
+    @if (session('status'))
+      <div class="alert alert-success">
+        {{ session('status') }}
+      </div>
+    @endif
 
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('password.email') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+    <div class="logo">
+      <a href="javascript:void(0);">Event<b>Scheduler</b></a>
     </div>
-</div>
+    <div class="card">
+      <div class="body">
+        <form id="sign_up" method="POST" action="{{ route('password.email') }}">
+          {{ csrf_field() }}
+          <div class="msg">Reset Password</div>
+          <div class="input-group">
+            <span class="input-group-addon">
+              <i class="material-icons">email</i>
+            </span>
+            <div class="form-line">
+              <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}" placeholder="Email Address" required autofocus>
+
+              @if ($errors->has('email'))
+              <span class="help-block">
+                <strong>{{ $errors->first('email') }}</strong>
+              </span>
+              @endif
+            </div>
+          </div>
+          <button class="btn btn-block btn-lg bg-pink waves-effect" type="submit">Send Password Reset Link</button>
+        </form>
+      </div>
+    </div>
+  </div>
+@endsection
+
+@section('footer')
+  <script src="{{ asset('js/jquery.validate.js') }}" charset="utf-8"></script>
+  <script src="{{ asset('js/sign-up.js') }}" charset="utf-8"></script>
 @endsection
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,76 +1,75 @@
-@extends('layouts.app')
+@extends('layouts.master')
+
+@section('style')
+  <link rel="stylesheet" href="{{ asset('css/waitMe.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/bootstrap-select.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/bootstrap-material-datetimepicker.css') }}">
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
+  <div class="signup-box">
+    @if (session('status'))
+      <div class="alert alert-success">
+        {{ session('status') }}
+      </div>
+    @endif
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('password.request') }}">
-                        {{ csrf_field() }}
-
-                        <input type="hidden" name="token" value="{{ $token }}">
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ $email or old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-
-                                @if ($errors->has('password_confirmation'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Reset Password
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+    <div class="logo">
+      <a href="javascript:void(0);">Event<b>Scheduler</b></a>
     </div>
-</div>
+    <div class="card">
+      <div class="body">
+        <form id="sign_up" method="POST" action="{{ route('password.request') }}">
+          {{ csrf_field() }}
+          <div class="msg">Reset Password</div>
+          <input type="hidden" name="token" value="{{ $token }}">
+          <div class="input-group">
+            <span class="input-group-addon">
+              <i class="material-icons">email</i>
+            </span>
+            <div class="form-line">
+              <input type="email" class="form-control" id="email" name="email" value="{{ $email or old('email') }}" placeholder="Email Address" required autofocus>
+              @if ($errors->has('email'))
+              <span class="help-block">
+                <strong>{{ $errors->first('email') }}</strong>
+              </span>
+              @endif
+            </div>
+          </div>
+          <div class="input-group">
+            <span class="input-group-addon">
+              <i class="material-icons">lock</i>
+            </span>
+            <div class="form-line">
+              <input type="password" class="form-control" id="password" name="password" required>
+              @if ($errors->has('password'))
+              <span class="help-block">
+                <strong>{{ $errors->first('password') }}</strong>
+              </span>
+              @endif
+            </div>
+          </div>
+          <div class="input-group">
+            <span class="input-group-addon">
+              <i class="material-icons">lock</i>
+            </span>
+            <div class="form-line">
+              <input type="password" class="form-control" id="confirm-password" name="password_confirmation" required>
+              @if ($errors->has('password_confirmation'))
+              <span class="help-block">
+                <strong>{{ $errors->first('password_confirmation') }}</strong>
+              </span>
+              @endif
+            </div>
+          </div>
+          <button class="btn btn-block btn-lg bg-pink waves-effect" type="submit">Reset Password</button>
+        </form>
+      </div>
+    </div>
+  </div>
+@endsection
+
+@section('footer')
+  <script src="{{ asset('js/jquery.validate.js') }}" charset="utf-8"></script>
+  <script src="{{ asset('js/sign-up.js') }}" charset="utf-8"></script>
 @endsection
