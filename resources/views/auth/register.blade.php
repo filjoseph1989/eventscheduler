f226207d71fa0d2882542c03d5482cb8d2541c8f
jlvoice777 <fil@greyandgreentech.com>
2017-06-28 13:22:02 +0800

    Return the inputs errors during registration failure

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.master')
 
-@section('page-title', 'user registration')
+@section('page-title', 'User Registration')
 
 @section('style')
   <link rel="stylesheet" href="{{ asset('css/waitMe.css') }}">
@@ -21,7 +21,7 @@
     @endif
 
     <div class="logo">
-      <a href="javascript:void(0);">Event<b>Scheduler</b></a> 
+      <a href="javascript:void(0);">Event<b>Scheduler</b></a>
     </div>
 
     <div class="card">
@@ -34,7 +34,7 @@
                         <i class="material-icons">person</i>
                     </span>
                     <div class="form-line">
-                        <input type="text" class="form-control" name="first_name" placeholder="Name" required autofocus>
+                        <input type="text" class="form-control" name="first_name" placeholder="First Name" value="{{ old('first_name') }}" required autofocus>
                         @if ($errors->has('first_name'))
                           <span class="help-block"> <strong>{{ $errors->first('first_name') }}</strong> </span>
                         @endif
@@ -45,7 +45,7 @@
                         <i class="material-icons">person</i>
                     </span>
                     <div class="form-line">
-                        <input type="text" class="form-control" name="last_name" placeholder="Last Name" required>
+                        <input type="text" class="form-control" name="last_name" placeholder="Last Name" value="{{ old('last_name') }}" required>
                         @if ($errors->has('last_name'))
                           <span class="help-block"> <strong>{{ $errors->first('last_name') }}</strong> </span>
                         @endif
@@ -56,7 +56,7 @@
                         <i class="material-icons">person</i>
                     </span>
                     <div class="form-line">
-                        <input type="text" class="form-control" name="middle_name" placeholder="Middle Name" required>
+                        <input type="text" class="form-control" name="middle_name" placeholder="Middle Name" value="{{ old('middle_name') }}" required>
                         @if ($errors->has('middle_name'))
                           <span class="help-block"> <strong>{{ $errors->first('middle_name') }}</strong> </span>
                         @endif
@@ -67,7 +67,7 @@
                         <i class="material-icons">person</i>
                     </span>
                     <div class="form-line">
-                        <input type="text" class="form-control" name="suffix_name" placeholder="Suffix">
+                        <input type="text" class="form-control" name="suffix_name" value="{{ old('suffix_name') }}" placeholder="Suffix">
                         @if ($errors->has('suffix_name'))
                           <span class="help-block"> <strong>{{ $errors->first('suffix_name') }}</strong> </span>
                         @endif
@@ -78,7 +78,7 @@
                         <i class="material-icons">email</i>
                     </span>
                     <div class="form-line">
-                        <input type="email" class="form-control" name="email" placeholder="Email Address" required>
+                        <input type="email" class="form-control" name="email" placeholder="Email Address" value="{{ old('email') }}" required>
                     </div>
                 </div>
                 <div class="input-group">
@@ -103,77 +103,6 @@
                     </span>
                     <div class="form-line">
                         <input id="account_number" type="text" class="form-control" name="account_number" placeholder="Account Number" value="{{ old('account_number') }}" required>
-                    </div>
-                </div>
-                <div class="input-group">
-                    <span class="input-group-addon">
-                        <i class="material-icons">lock</i>
-                    </span>
-                    <div class="form-line">
-                      <select class="form-control show-tick" name="user_account_id">
-                        <option value="0">-- Account Type --</option>
-                        <option value="1">Admin</option>
-                        <option value="2">Organization Adviser</option>
-                        <option value="3">Organization Head</option>
-                        <option value="4">Organization Member</option>
-                        <option value="5">OSA Personnel</option>
-                      </select>
-                      @if ($errors->has('user_account_id'))
-                        <span class="help-block"> <strong>{{ $errors->first('user_account_id') }}</strong> </span>
-                      @endif
-                    </div>
-                </div>
-                <div class="input-group">
-                    <span class="input-group-addon">
-                        <i class="material-icons">lock</i>
-                    </span>
-                    <div class="form-line">
-                        <select class="form-control show-tick" name="course_id">
-                            <option value="0">-- Course --</option>
-                            <option value="1">BS in Computer Science</option>
-                            <option value="2">Food Technology</option>
-                            <option value="3">BS Biology</option>
-                            <option value="4">BACA</option>
-                            <option value="5">BA Anthropology</option>
-                        </select>
-                    </div>
-                </div>
-
-                <div class="input-group">
-                    <span class="input-group-addon">
-                        <i class="material-icons">lock</i>
-                    </span>
-                    <div class="form-line">
-                        <select class="form-control show-tick" name="department_id">
-                          <option value="0">-- Department --</option>
-                          <option value="1">College of Science and Mathematics</option>
-                          <option value="2">College of Humanities and Social Science</option>
-                          <option value="3">Department of Architecture</option>
-                          <option value="4">School of Management</option>
-                        </select>
-                    </div>
-                </div>
-                <div class="input-group">
-                    <span class="input-group-addon">
-                        <i class="material-icons">accessibility</i>
-                    </span>
-                    <div class="form-line">
-                        <select class="form-control show-tick" name="position_id">
-                          <option value="0">-- Positions --</option>
-                          <option value="1">Chairperson</option>
-                          <option value="2">Vice Chairperson</option>
-                          <option value="3">President</option>
-                          <option value="4">Vice President</option>
-                          <option value="5">Secretary</option>
-                          <option value="6">Treasurer</option>
-                          <option value="7">Pubic Relation Officer</option>
-                          <option value="8">1st Year Representative</option>
-                          <option value="9">2nd Year Representative</option>
-                          <option value="10">3rd Year Representative</option>
-                          <option value="11">Instructor</option>
-                          <option value="12">OSA Staff</option>
-                          <option value="12">OSA Head</option>
-                        </select>
                     </div>
                 </div>
                 <div class="input-group">
@@ -206,6 +135,76 @@
                     </span>
                     <div class="form-line">
                         <input id="mobile_number" type="text" class="form-control" name="mobile_number" placeholder="Modile Number" value="{{ old('mobile_number') }}" required>
+                    </div>
+                </div>
+                <div class="input-group">
+                    <span class="input-group-addon">
+                        <i class="material-icons">lock</i>
+                    </span>
+                    <div class="form-line">
+                        <select class="form-control show-tick" name="user_account_id">
+                            <option value="0">-- Account Type --</option>
+                            <option value="1">Admin</option>
+                            <option value="2">Organization Adviser</option>
+                            <option value="3">Organization Head</option>
+                            <option value="4">Organization Member</option>
+                            <option value="5">OSA Personnel</option>
+                        </select>
+                        @if ($errors->has('user_account_id'))
+                        <span class="help-block"> <strong>{{ $errors->first('user_account_id') }}</strong> </span>
+                        @endif
+                    </div>
+                </div>
+                <div class="input-group">
+                    <span class="input-group-addon">
+                        <i class="material-icons">lock</i>
+                    </span>
+                    <div class="form-line">
+                        <select class="form-control show-tick" name="course_id">
+                            <option value="0">-- Course --</option>
+                            <option value="1">BS in Computer Science</option>
+                            <option value="2">Food Technology</option>
+                            <option value="3">BS Biology</option>
+                            <option value="4">BACA</option>
+                            <option value="5">BA Anthropology</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="input-group">
+                    <span class="input-group-addon">
+                        <i class="material-icons">lock</i>
+                    </span>
+                    <div class="form-line">
+                        <select class="form-control show-tick" name="department_id">
+                            <option value="0">-- Department --</option>
+                            <option value="1">College of Science and Mathematics</option>
+                            <option value="2">College of Humanities and Social Science</option>
+                            <option value="3">Department of Architecture</option>
+                            <option value="4">School of Management</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="input-group">
+                    <span class="input-group-addon">
+                        <i class="material-icons">accessibility</i>
+                    </span>
+                    <div class="form-line">
+                        <select class="form-control show-tick" name="position_id">
+                            <option value="0">-- Positions --</option>
+                            <option value="1">Chairperson</option>
+                            <option value="2">Vice Chairperson</option>
+                            <option value="3">President</option>
+                            <option value="4">Vice President</option>
+                            <option value="5">Secretary</option>
+                            <option value="6">Treasurer</option>
+                            <option value="7">Pubic Relation Officer</option>
+                            <option value="8">1st Year Representative</option>
+                            <option value="9">2nd Year Representative</option>
+                            <option value="10">3rd Year Representative</option>
+                            <option value="11">Instructor</option>
+                            <option value="12">OSA Staff</option>
+                            <option value="12">OSA Head</option>
+                        </select>
                     </div>
                 </div>
                 <button class="btn btn-block btn-lg bg-pink waves-effect" type="submit">SIGN UP</button>
