6e0e1bb0916e7c39760e65707b477b7eadd22197
jlvoice777 <fil@greyandgreentech.com>
2017-07-02 15:19:57 +0800

    Working on user sidebar

--- resources/views/components/user-sidebar/admin-menu.blade.php
+++ resources/views/components/user-sidebar/admin-menu.blade.php
@@ -3,3 +3,43 @@
  * This will display the menus for admin account
  */
 ?>
+<li>
+  <a href="#" class="menu-toggle">
+    <i class="material-icons">supervisor_account</i>
+    <span>Organization</span>
+  </a>
+  <ul class="ml-menu">
+    <li>
+      <a href="#">
+        <span>List of Organization</span>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <span>Upcomming Events</span>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <span>Generate Attendance</span>
+      </a>
+    </li>
+  </ul>
+</li>
+<li>
+  <a href="#" class="menu-toggle">
+    <i class="material-icons">supervisor_account</i>
+    <span>Manage Member</span>
+  </a>
+  <ul class="ml-menu">
+    <li>
+      <a href="#">
+        <span>Add New Member</span>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <span>All Members</span>
+      </a>
+    </li>
+</li>
