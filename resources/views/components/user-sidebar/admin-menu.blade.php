dd629a09de83f0fbd288c116bc770b63f7f964e9
Liz N. De Guzman <janicalizdeguzman@gmail.com>
2017-07-03 01:19:52 +0800

    naay issue sa routing

--- resources/views/components/user-sidebar/admin-menu.blade.php
+++ resources/views/components/user-sidebar/admin-menu.blade.php
@@ -1,5 +1,49 @@
 <?php
 /**
+* This will display the menus for the user account
+*/
+?>
+<li>
+  <li>
+    <a href="#">
+      <i class="material-icons">assignment_turned_in</i>
+      <span>REGISTRATION</span>
+    </a>
+  </li>
+ <a href="#" class="menu-toggle">
+   <i class="material-icons">input</i>
+   <span>Insert Data</span>
+ </a>
+ <ul class="ml-menu">
+   <li>
+     <a href="#">
+       <span>USERS</span>
+     </a>
+   </li>
+   <li>
+     <a href="#">
+       <span>COURSES</span>
+     </a>
+   </li>
+   <li>
+     <a href="#">
+       <span>DEPARTMENTS</span>
+     </a>
+   </li>
+   <li>
+     <a href="#">
+       <span>POSITIONS</span>
+     </a>
+   </li>
+   <li>
+     <a href="#">
+       <span>APPROVERS</span>
+     </a>
+   </li>
+ </ul>
+</li>
+<?php
+/**
  * This will display the menus for admin account
  */
 ?>
--- routes/web.php
+++ routes/web.php
@@ -13,32 +13,11 @@ Route::get('/', function () {
   return redirect()->route('login');
 });
 Auth::routes();
+
+#revalidation of back history
 Route::group(['middleware' => 'revalidate'], function()
 {
-  Route::name('home')->get('/home', 'HomeController@index');
-  Route::prefix('users')->group(function() {
-    Route::name('user.registered')->post('/registered', 'UserController@create');
 
-    Route::name('user.account.register')->get('/register', 'UsersAccountAdminController@showRegisterForm');
-    Route::name('user.account.registered')->post('/account/registered', 'UsersAccountAdminController@create');
-
-    Route::name('user.course.register')->get('/course/register', 'CourseController@showRegisterForm');
-    Route::name('user.course.registered')->post('/course/registered', 'CourseController@create');
-
-    Route::name('user.department.register')->get('/department/register', 'DepartmentController@showRegisterForm');
-    Route::name('user.department.registered')->post('/department/registered', 'DepartmentController@create');
-
-    Route::name('user.position.register')->get('/position/register', 'PositionController@showRegisterForm');
-    Route::name('user.position.registered')->post('/position/registered', 'PositionController@create');
-
-    Route::name('user.organization.register')->get('/organization/register', 'OrganizationController@showRegisterForm');
-    Route::name('user.organization.registered')->post('/organization/registered', 'OrganizationController@create');
-
-    Route::name('user.event-category.register')->get('/event-category/register', 'EventCategoryController@showRegisterForm');
-    Route::name('user.event-category.registered')->post('/event-category/registered', 'EventCategoryController@create');
-
-    Route::name('user.logout')->get('/logout', 'Auth\LoginController@userLogout');
-  });
   /*
   |--------------------------------------------------------------------------
   | Admin route
@@ -113,14 +92,18 @@ Route::group(['middleware' => 'revalidate'], function()
   });
   /*
   |--------------------------------------------------------------------------
+  | Users Routes
+  |--------------------------------------------------------------------------
+  */
+
+    Route::name('home')->get('/home', 'HomeController@index');
+    Route::prefix('users')->group(function() {});
+    Route::name('user.logout')->get('/logout', 'Auth\LoginController@userLogout');
+});
+  /*
+  |--------------------------------------------------------------------------
   | Notification route
   |--------------------------------------------------------------------------
   */
   Route::name('notify.via.sms')->get('/notify_via_sms', 'smsNotifierController@index');
   Route::name('faceboo.notification')->get('/fb/post', 'HomeController@sendNotification');
-});
-/*
-|--------------------------------------------------------------------------
-| Users Routes
-|--------------------------------------------------------------------------
-*/
