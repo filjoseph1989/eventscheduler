bd6625b59189738964405b3a63fc2d403f42d675
Liz N. De Guzman <jose.quinonez@hpoutsourcinginc.com>
2017-07-07 15:49:52 +0800

    Improve the organization head sidebar and remove the calendar when in home page

--- resources/views/components/user-sidebar/organization-head-menu.blade.php
+++ resources/views/components/user-sidebar/organization-head-menu.blade.php
@@ -4,25 +4,46 @@
  */
 ?>
 <li>
-  <a href="#" class="menu-toggle">
+  <a href="javascript:void(0);" class="menu-toggle">
     <i class="material-icons">supervisor_account</i>
-    <span>Action</span>
+    <span>Actions</span>
   </a>
   <ul class="ml-menu">
     <li>
-      <a href="#">
+      <a href="javascript:void(0);" class="menu-toggle">
         <span>Manage Schedule</span>
       </a>
+      <ul class="ml-menu">
+        <li>
+          <a href="#">Show Calendar</a>
+        </li>
+        <li>
+          <a href="#">Generate Attendance</a>
+        </li>
+      </ul>
     </li>
     <li>
-      <a href="#">
-        <span>Generate Attendance</span>
+      <a href="javascript:void(0);" class="menu-toggle">
+        <span>Events</span>
       </a>
+      <ul class="ml-menu">
+        <li>
+          <a href="#">Create Events</a>
+        </li>
+        <li>
+          <a href="#">Manage Notifications</a>
+        </li>
+      </ul>
     </li>
     <li>
-      <a href="#">
-        <span>Manage Notification</span>
+      <a href="javascript:void(0);" class="menu-toggle">
+        <span>Organizations</span>
       </a>
+      <ul class="ml-menu">
+        <li>
+          <a href="#">List Of Organizations</a>
+        </li>
+      </ul>
     </li>
   </ul>
 </li>
