830436de91c6921f33cf93073f88b24bed8c201a
Liz N. De Guzman <fil@greyandgreentech.com>
2017-07-26 12:29:25 +0800

    Update the migration for creating personal event

--- database/migrations/2017_07_16_182305_create_personal_events_table.php
+++ database/migrations/2017_07_16_182305_create_personal_events_table.php
@@ -15,11 +15,11 @@ class CreatePersonalEventsTable extends Migration
     {
         Schema::create('personal_events', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('event_type_id')->unsigned()->index();
-            $table->integer('event_category_id')->unsigned()->index();
             $table->integer('user_id')->unsigned()->index();
-            $table->integer('calendar_id')->unsigned()->index();
-            $table->string('event');
+            $table->integer('event_type_id')->unsigned()->index();
+            $table->enum('category', ['Public', 'Private']);
+            $table->enum('semester', ['First Semester', 'Second Semester']);
+            $table->string('title');
             $table->text('description');
             $table->string('venue');
             $table->date('date_start')->useCurrent();
@@ -27,15 +27,23 @@ class CreatePersonalEventsTable extends Migration
             $table->time('date_start_time')->default('00:00:00');
             $table->time('date_end_time')->default('00:00:00');
             $table->integer('whole_day');
+            $table->integer('notify_via_twitter')->default(0);
+            $table->integer('notify_via_facebook')->default(0);
+            $table->integer('notify_via_sms')->default(0);
+            $table->integer('notify_via_email')->default(0);
+            $table->string('additional_msg_facebook')->nullable();
+            $table->string('additional_msg_sms')->nullable();
+            $table->string('additional_msg_email')->nullable();
+            $table->string('picture_facebook')->nullable();
+            $table->string('picture_twitter')->nullable();
+            $table->string('picture_email')->nullable();
             $table->tinyInteger('status')->default(0);
             $table->softDeletes();
             $table->timestamps();
 
             # Foreign keys
-            $table->foreign('event_type_id')->references('id')->on('event_types');
-            $table->foreign('event_category_id')->references('id')->on('event_categories');
             $table->foreign('user_id')->references('id')->on('users');
-            $table->foreign('calendar_id')->references('id')->on('calendars');
+            $table->foreign('event_type_id')->references('id')->on('event_types');
         });
     }
 
