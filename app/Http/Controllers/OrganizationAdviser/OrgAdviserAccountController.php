ceb3c0e8c4314bde7c559746416fedd21ab80aa5
Liz <fil@greyandgreentech.com>
2017-07-30 23:01:57 +0800

    Remove unused method

--- app/Http/Controllers/OrganizationAdviser/OrgAdviserAccountController.php
+++ app/Http/Controllers/OrganizationAdviser/OrgAdviserAccountController.php
@@ -34,61 +34,6 @@ class OrgAdviserAccountController extends Controller
     $this->middleware('web');
   }
 
-  /*
-    Issue 38: Review methods below if still in use
-   */
-
-  /**
-   * Display a list of organization.
-   *
-   * @return \Illuminate\Http\Response
-   */
-  public function myOrganization()
-  {
-    # Is the user login?
-    parent::loginCheck();
-
-    # Is the user an organization adviser?
-    if (parent::isOrgAdviser()) {
-      # Issue 31: Change this to eloquent way of getting data
-      # Get the organization base onn the user's ID
-      $organization = OrganizationGroup::select(
-        'organizations.id',
-        'organizations.name'
-      )
-      ->join('organizations', 'organizations.id', '=', 'organization_groups.organization_id')
-      ->where('organization_groups.user_id', '=', Auth::user()->id)
-      ->get();
-
-      # View
-      $login_type = "user";
-      return view(
-        'pages/users/organization-adviser/manage_schedule/my-organization',
-        compact(
-          'login_type',
-          'organization'
-        )
-      );
-    } else {
-      return redirect()->route('home');
-    }
-  }
-
-  /**
-   * Display the personal calendar
-   *
-   * @return
-   */
-  public function myPersonalCalendar()
-  {
-    # Check if the user is loggedin
-    parent::loginCheck();
-
-    if (parent::isOrgAdviser()) {
-      return view('pages/users/organization-adviser/calendars/my-personal-calendar');
-    }
-  }
-
   /**
    * A private method where it determine if the user currently
    * loggedin in the system is an adviser, return home if not.
@@ -135,5 +80,24 @@ class OrgAdviserAccountController extends Controller
   public function isAdviserInGivenOrganization($id)
   {
     $adviser = OrganizationAdviserGroup::where('organization_id', '=', $id)->get();
+  }
+
+  /*
+    Issue 38: Review methods below if still in use
+   */
+
+  /**
+   * Display the personal calendar
+   *
+   * @return
+   */
+  public function myPersonalCalendar()
+  {
+    # Check if the user is loggedin
+    parent::loginCheck();
+
+    if (parent::isOrgAdviser()) {
+      return view('pages/users/organization-adviser/calendars/my-personal-calendar');
+    }
   }
 }
