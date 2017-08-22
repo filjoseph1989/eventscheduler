53845382698857c9b747f9e471b05ba1f437ebc2
Liz N <jose.quinonez@hpoutsourcinginc.com>
2017-08-22 18:39:34 +0800

    Updated the eventTtype seeder

--- database/seeds/EventTypesTableSeeder.php
+++ database/seeds/EventTypesTableSeeder.php
@@ -38,6 +38,106 @@ class EventTypesTableSeeder extends Seeder
                 'created_at'     => Carbon::now()->toDateTimeString(),
                 'updated_at'     => Carbon::now()->toDateTimeString(),
             ),
+            array(
+                'name'           => 'Sample 2',
+                'created_at'     => Carbon::now()->toDateTimeString(),
+                'updated_at'     => Carbon::now()->toDateTimeString(),
+            ),
+            array(
+                'name'           => 'Sample 3',
+                'created_at'     => Carbon::now()->toDateTimeString(),
+                'updated_at'     => Carbon::now()->toDateTimeString(),
+            ),
+            array(
+                'name'           => 'Sample 4',
+                'created_at'     => Carbon::now()->toDateTimeString(),
+                'updated_at'     => Carbon::now()->toDateTimeString(),
+            ),
+            array(
+                'name'           => 'Sample 5',
+                'created_at'     => Carbon::now()->toDateTimeString(),
+                'updated_at'     => Carbon::now()->toDateTimeString(),
+            ),
+            array(
+                'name'           => 'Sample 6',
+                'created_at'     => Carbon::now()->toDateTimeString(),
+                'updated_at'     => Carbon::now()->toDateTimeString(),
+            ),
+            array(
+                'name'           => 'Sample 7',
+                'created_at'     => Carbon::now()->toDateTimeString(),
+                'updated_at'     => Carbon::now()->toDateTimeString(),
+            ),
+            array(
+                'name'           => 'Sample 8',
+                'created_at'     => Carbon::now()->toDateTimeString(),
+                'updated_at'     => Carbon::now()->toDateTimeString(),
+            ),
+            array(
+                'name'           => 'Sample 9',
+                'created_at'     => Carbon::now()->toDateTimeString(),
+                'updated_at'     => Carbon::now()->toDateTimeString(),
+            ),
+            array(
+                'name'           => 'Sample 10',
+                'created_at'     => Carbon::now()->toDateTimeString(),
+                'updated_at'     => Carbon::now()->toDateTimeString(),
+            ),
+            array(
+                'name'           => 'Sample 11',
+                'created_at'     => Carbon::now()->toDateTimeString(),
+                'updated_at'     => Carbon::now()->toDateTimeString(),
+            ),
+            array(
+                'name'           => 'Sample 12',
+                'created_at'     => Carbon::now()->toDateTimeString(),
+                'updated_at'     => Carbon::now()->toDateTimeString(),
+            ),
+            array(
+                'name'           => 'Sample 13',
+                'created_at'     => Carbon::now()->toDateTimeString(),
+                'updated_at'     => Carbon::now()->toDateTimeString(),
+            ),
+            array(
+                'name'           => 'Sample 14',
+                'created_at'     => Carbon::now()->toDateTimeString(),
+                'updated_at'     => Carbon::now()->toDateTimeString(),
+            ),
+            array(
+                'name'           => 'Sample 15',
+                'created_at'     => Carbon::now()->toDateTimeString(),
+                'updated_at'     => Carbon::now()->toDateTimeString(),
+            ),
+            array(
+                'name'           => 'Sample 16',
+                'created_at'     => Carbon::now()->toDateTimeString(),
+                'updated_at'     => Carbon::now()->toDateTimeString(),
+            ),
+            array(
+                'name'           => 'Sample 17',
+                'created_at'     => Carbon::now()->toDateTimeString(),
+                'updated_at'     => Carbon::now()->toDateTimeString(),
+            ),
+            array(
+                'name'           => 'Sample 18',
+                'created_at'     => Carbon::now()->toDateTimeString(),
+                'updated_at'     => Carbon::now()->toDateTimeString(),
+            ),
+            array(
+                'name'           => 'Sample 19',
+                'created_at'     => Carbon::now()->toDateTimeString(),
+                'updated_at'     => Carbon::now()->toDateTimeString(),
+            ),
+            array(
+                'name'           => 'Sample 20',
+                'created_at'     => Carbon::now()->toDateTimeString(),
+                'updated_at'     => Carbon::now()->toDateTimeString(),
+            ),
+            array(
+                'name'           => 'Sample 21',
+                'created_at'     => Carbon::now()->toDateTimeString(),
+                'updated_at'     => Carbon::now()->toDateTimeString(),
+            ),
         ]);
     }
 }
