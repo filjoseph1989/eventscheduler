6e0e1f239f4731d0ef78cec45ffbdad73de5a20e
jlvoice777 <fil@greyandgreentech.com>
2017-09-02 22:24:55 +0800

    Updated organizationTableSeeder by correcting the url

--- database/seeds/OrganizationsTableSeeder.php
+++ database/seeds/OrganizationsTableSeeder.php
@@ -13,40 +13,40 @@ class OrganizationsTableSeeder extends Seeder
     {
       DB::table('organizations')->insert([
         array(
-          'name' => 'Organization',
-          'description' => 'No description yet',
+          'name'         => 'Organization',
+          'description'  => 'No description yet',
           'date_started' => date('Y-m-d H:i:s'),
           'date_expired' => date('Y-m-d H:i:s'),
-          'url' => '',
-          'created_at' => date('Y-m-d H:i:s'),
-          'updated_at' => date('Y-m-d H:i:s')
+          'url'          => '',
+          'created_at'   => date('Y-m-d H:i:s'),
+          'updated_at'   => date('Y-m-d H:i:s')
         ),
         array(
-          'name' => 'Alpha Phi Omega',
-          'description' => 'No description yet',
+          'name'         => 'Alpha Phi Omega',
+          'description'  => 'No description yet',
           'date_started' => date('Y-m-d H:i:s'),
           'date_expired' => date('Y-m-d H:i:s'),
-          'url' => 'http://apo.org.ph',
-          'created_at' => date('Y-m-d H:i:s'),
-          'updated_at' => date('Y-m-d H:i:s')
+          'url'          => 'http://apo.org.ph',
+          'created_at'   => date('Y-m-d H:i:s'),
+          'updated_at'   => date('Y-m-d H:i:s')
         ),
         array(
-          'name' => 'Omega Alpha',
-          'description' => 'No description yet',
+          'name'         => 'Omega Alpha',
+          'description'  => 'No description yet',
           'date_started' => date('Y-m-d H:i:s'),
           'date_expired' => date('Y-m-d H:i:s'),
-          'url' => 'oa.org.ph',
-          'created_at' => date('Y-m-d H:i:s'),
-          'updated_at' => date('Y-m-d H:i:s')
+          'url'          => 'http://oa.org.ph',
+          'created_at'   => date('Y-m-d H:i:s'),
+          'updated_at'   => date('Y-m-d H:i:s')
         ),
         array(
-          'name' => 'Pi Sigma',
-          'description' => 'No description yet',
+          'name'         => 'Pi Sigma',
+          'description'  => 'No description yet',
           'date_started' => date('Y-m-d H:i:s'),
           'date_expired' => date('Y-m-d H:i:s'),
-          'url' => 'pi.org.ph',
-          'created_at' => date('Y-m-d H:i:s'),
-          'updated_at' => date('Y-m-d H:i:s')
+          'url'          => 'http://pi.org.ph',
+          'created_at'   => date('Y-m-d H:i:s'),
+          'updated_at'   => date('Y-m-d H:i:s')
         ),
       ]);
     }
