75fdafd5cae29750f42dfb2456e63f9578d0717d
jlvoice777 <fil@greyandgreentech.com>
2017-09-04 10:08:00 +0800

    Nag initiate nako og task scheduler para mag send og reminder on weekly bases and 1 day ahead to email and phone.
    Please read comment on SendNotification on line 38 for details of the steps.
    
    This scheduler is only applicable to email and mobile phone, why?
    
    kay dili pwede mag balik balik og notification dapat sa facebook and twitter, dili sila gusto ana hehe

---
+++ app/Console/Commands/SendNotifications.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class SendNotifications extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'SendNotifications:notification';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This will send notification about upcomming events to facebook, twitter, email and mobile';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        /**
+         * Steps to take 
+         * 
+         * I think, this functionality should be in mobile and email, because
+         * notification about the event is already posted on social media the an event is approved,
+         * and social prevent same notification over and over -- haha
+         * 
+         * 1. get the upcomming approve events
+         * 2. get the organizer of the events (e.g Alpha Pi Omega)
+         * 3. get the member of the events and other attendees base on the user attendance table
+         *      - Get users on attendance table where status is true
+         * 4. Given the data and time from the events send notification every week and 1 day prior to the event
+         *      - Notification on weekly bases, the date of sending notification must be less than the date of the event
+         *      - Similar for the day ahead, the date must be less than the date of the event
+         */
+    }
+}
