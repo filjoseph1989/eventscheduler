c93ab6cda3988d47b65a07cc9241cd527e4a3df2
jlvoice777 <fil@greyandgreentech.com>
2017-09-06 00:22:56 +0800

    Commit nako ni kay para maka backtrack ko sa email template

--- resources/views/emails/mail.blade.php
+++ resources/views/emails/mail.blade.php
@@ -5,6 +5,13 @@
     <title> </title>
   </head>
   <body style="background: black; color: white">
-    <p>{{$notification_message}}</p>
+    <p>Title: {{ $event->title }}</p>
+    <p>Description: {{ $event->description }}</p>
+    <p>Venue: {{ $event->venue }}</p>
+    <p>Organizer: {{ $event->organization->name }}</p>
+    <p>Start Date: {{ date('Y M d', strtotime($event->date_start)) }}</p>
+    <p>Time: {{ $event->date_start_time }}</p>
+    <p>End Date: {{ date('Y M d', strtotime($event->date_end)) }}</p>
+    <p>Time: {{ $event->date_end_time }}</p>
   </body>
 </html>
