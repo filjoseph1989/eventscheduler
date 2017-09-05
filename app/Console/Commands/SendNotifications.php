cf77c7a8ffa5a2381e97a1cd0f4e4f57480d6f9d
Liz N <jose.quinonez@hpoutsourcinginc.com>
2017-09-05 21:33:08 +0800

    Commit sa ko daan before mag proceed,
    Nabuhat na nako ang mag email sa user base sa event na upcoming og member pud siya sa organization na nag organize ato na event including those user registered in user_attendance table.
    
    nganu apil ang user_attendance table?
    
    kay naa users dinha nga dili member sa organization na nag organize pero gusto mo attend sa event.

--- app/Console/Commands/SendNotifications.php
+++ app/Console/Commands/SendNotifications.php
@@ -5,6 +5,9 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Http\Controllers\MailController;
 
+# Models
+use App\Models\Event;
+
 class SendNotifications extends Command
 {
     /**
@@ -34,27 +37,26 @@ class SendNotifications extends Command
     /**
      * Execute the console command.
      *
+     * The functionality here is intended only for
+     * email and mobile only, because facebook and twitter doesn't allow
+     * posting same post
+     *
      * @return mixed
      */
     public function handle()
     {
-        /**
-         * Steps to take
-         *
-         * I think, this functionality should be in mobile and email only, because
-         * notification about the event is already posted on social media the an event is approved,
-         * and social prevent same notification over and over -- haha
-         *
-         * 1. get the upcomming approve events
-         * 2. get the organizer of the events (e.g Alpha Pi Omega)
-         * 3. get the member of the events and other attendees base on the user attendance table
-         *      - Get users on attendance table where status is true
-         * 4. Given the data and time from the events send notification every week and 1 day prior to the event
-         *      - Notification on weekly bases, the date of sending notification must be less than the date of the event
-         *      - Similar for the day ahead, the date must be less than the date of the event
-         */
-        $mail = new MailController();
-        $mail->index();
-
+      /**
+       * Steps to take
+       *
+       * 1. get the upcomming approve events
+       * 2. get the organizer of the events (e.g Alpha Pi Omega)
+       * 3. get the member of the events and other attendees base on the user attendance table
+       *      - Get users on attendance table where status is true
+       * 4. Given the data and time from the events send notification every week and 1 day prior to the event
+       *      - Notification on weekly bases, the date of sending notification must be less than the date of the event
+       *      - Similar for the day ahead, the date must be less than the date of the event
+       */ 
+      $mail = new MailController();
+      $mail->index();
     }
 }
--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -9,16 +9,72 @@ use Illuminate\Support\Facades\Mail;
 # MAiler
 use App\Mail\Mailtrap;
 use App\Mail\EmailNotification;
+
+# Models
 use App\Models\User;
+use App\Models\Event;
+use App\Models\OrganizationGroup;
+use App\Models\UserAttendance;
 
 class MailController extends Controller
 {
+    private $emails = [];
+
+    /**
+     * Build object
+     */
+    public function __construct() {
+
+    }
     /**
      * Send email
      * @return
      */
-    public function index(){
-      $user = User::find(1);
-      Mail::to($user)->send(new EmailNotification());
+    public function index() {
+      # Get the approve events within this year, this comming month
+      # and this coming days with the event organizer (e.g APO)
+      $event = Event::with('organization')
+        ->where('status', 'upcoming')
+        ->where('approve_status', 'approved')
+        ->whereYear("date_start", '=', date('Y'))
+        ->whereMonth('date_start','>=',date('m'))
+        ->whereDay('date_start', '>', date('d'))
+        ->get();
+
+      # From the given organization ID for each event
+      # get the members
+      foreach ($event as $key => $value) {
+        $organizationGroup = OrganizationGroup::with('user')
+          ->where('organization_id', $value->organization->id)
+          ->get();
+
+        self::gatheringEmails($organizationGroup);
+
+        $attendance = UserAttendance::with('user')
+          ->where('event_id', $value->id)
+          ->where('status', 'true')
+          ->get();
+
+        self::gatheringEmails($attendance);
+
+        # Send an email to user
+        foreach ($this->emails as $key => $email) {
+          Mail::to($email)->send(new EmailNotification());
+        }
+      }
+
+    }
+
+    /**
+     * The purpose for this method is to get email from the users
+     * @return [type] [description]
+     */
+    private function gatheringEmails($data)
+    {
+      foreach ($data as $key => $value) {
+        if (! isset($this->emails[$value->user->id])) {
+          $this->emails[$value->user->id] = $value->user->email;
+        }
+      }
     }
 }
