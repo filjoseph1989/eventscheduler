b8933c36c564a546f73c32fbd147c929e8905c83
jlvoice777 <fil@greyandgreentech.com>
2017-09-14 22:07:24 +0800

    Remove one condition, to prevent it from sending reminder everyday

--- app/Http/Controllers/OsaPersonnel/UserController.php
+++ app/Http/Controllers/OsaPersonnel/UserController.php
@@ -15,8 +15,8 @@ use App\Models\Position;
  * @author Liz <janicalizdeguzman@gmail.com>
  * @since 0.1
  * @version 0.1
- * @created 09-13-2017
- * @updated 09-13-2017
+ * @created 8-26-2017
+ * @updated 8-26-2017
  */
 class UserController extends Controller
 {
@@ -31,13 +31,27 @@ class UserController extends Controller
   }
 
   /**
-   * return the list of users
+   * This method search teh user table to be used
+   * by organization head to add new organization memeber
    *
-   * @return void
+   * @param  Request $data
+   * @return Illuminate\Response
    */
-  public function getUser()
+  public function search(Request $data)
   {
-    $userList = User::where()
-  }
+    $user = User::with(['course', 'department'])
+      ->where('account_number', 'LIKE', '%' . $data->search . '%')
+      ->get();
 
+    $position = Position::all();
+
+    if ($user->count() != 0) {
+      $login_type = 'user';
+      return view('pages/users/osa-personnel/members/list', compact(
+        'user', 'login_type', 'position'
+      ));
+    }
+
+    return back()->with('status_warning', 'Sorry We couldn\'t find what your looking for.');
+  }
 }
