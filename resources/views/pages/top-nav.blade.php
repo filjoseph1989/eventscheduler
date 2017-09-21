ff63b15058a1b1385fffaf98880851eaf1aefe68
Liz N <jose.quinonez@hpoutsourcinginc.com>
2017-09-21 17:13:41 +0800

    Enable search navigation

--- resources/views/pages/top-nav.blade.php
+++ resources/views/pages/top-nav.blade.php
@@ -34,7 +34,7 @@
     </div>
     <div class="collapse navbar-collapse" id="navbar-collapse">
       <ul class="nav navbar-nav navbar-right">
-        <!-- <li><a href="#" class="js-search" data-close="true"><i class="material-icons">search</i></a></li> -->
+        <li><a href="#" class="js-search" data-close="true"><i class="material-icons">search</i></a></li>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">
             <i class="material-icons">notifications</i>
