7bbee81339592398cc2d76ccf683f5b2dfa43241
Liz N. De Guzman <janicalizdeguzman@gmail.com>
2017-09-28 02:27:29 +0800

    renamed tables from user_account_type to user_type and from user_attendance to attendance

--- database/migrations/2017_07_06_030612_create_user_type_table.php
+++ database/migrations/2017_07_06_030612_create_user_type_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUserAccountsTable extends Migration
+class CreateUserTypeTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateUserAccountsTable extends Migration
      */
     public function up()
     {
-        Schema::create('user_accounts', function (Blueprint $table) {
+        Schema::create('user_types', function (Blueprint $table) {
           $table->increments('id');
           $table->string('name')->unique();
           $table->string('theme')->nullable();
