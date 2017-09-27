c69fa33535e9f4a492751530bf7ae578597cdb2c
jlvoice777 <fil@greyandgreentech.com>
2017-09-27 22:46:28 +0800

    Added a modal for event information and configuration.

--- resources/views/events-list.blade.php
+++ resources/views/events-list.blade.php
@@ -18,6 +18,7 @@
   <link href="{{ asset('css/animate.css') }}?v=1" rel="stylesheet">
   <link href="{{ asset('css/style.css') }}?v=1.0.1" rel="stylesheet">
   <link href="{{ asset('css/dataTables.bootstrap.css') }}?v=1" rel="stylesheet">
+  <link href="{{ asset('css/bootstrap-select.css') }}?v=1" rel="stylesheet">
 
   {{-- Remove Me --}}
   <link href="{{ asset('css/all-themes.css') }}" rel="stylesheet">
@@ -56,36 +57,33 @@
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                   <table class="table table-bordered table-striped table-hover js-basic-example dataTable">
                     <thead>
-                      <th><a href="#">Organization Name</a></th>
-                      <th>Abbrivation</th>
-                      <th>Leader</th>
+                      <th><a href="#">Title</a></th>
+                      <th>Venue</th>
+                      <th>Organizer</th>
+                      <th>Start</th>
+                      <th>End</th>
                       <th>Status</th>
+                      <th>Action</th>
                     </thead>
                     <tbody>
                       <tr>
-                        <td><a href="#" data-target="#org-profile" data-toggle="modal">Computer Science Society</a></td>
-                        <td>CSS</td>
-                        <td><a href="#">Mark Zuckerberg</a></td>
-                        <td>Active</td>
-                      </tr>
-                      <tr>
-                        <td><a href="#" data-target="#org-profile" data-toggle="modal">Association of IT students</a></td>
-                        <td>AITS</td>
-                        <td><a href="#">Donald Trump</a></td>
-                        <td>Active</td>
-                      </tr>
-                      <tr>
-                        <td><a href="#" data-target="#org-profile" data-toggle="modal">Association of Engineering students</a></td>
-                        <td>AES</td>
-                        <td><a href="#">Elon Musk</a></td>
-                        <td>Active</td>
+                        <td><a href="#" data-target="#event" data-toggle="modal">Kadayawan sa UP</a></td>
+                        <td>UP Gym</td>
+                        <td>Computer Science Society</td>
+                        <td>Oct 01, 2017 @ 10:00 AM</td>
+                        <td>Oct 01, 2017 @ 05:00 AM</td>
+                        <td>Upcoming</td>
+                        <td><a href="#">Attend</a>|<a href="#">Decline</a></td>
                       </tr>
                     </tbody>
                     <tfoot>
-                      <th>Organization Name</th>
-                      <th>Abbrivation</th>
-                      <th>Leader</th>
+                      <th><a href="#">Title</a></th>
+                      <th>Venue</th>
+                      <th>Organizer</th>
+                      <th>Start</th>
+                      <th>End</th>
                       <th>Status</th>
+                      <th>Action</th>
                     </tfoot>
                   </table>
                 </div>
@@ -96,43 +94,127 @@
       </div>
     </div>
   </section>
-  <div id="org-profile" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div id="event" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="event" aria-hidden="true">
     <div class="modal-dialog modal-lg">
       <div class="modal-content">
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
-          <h4 class="modal-title" id="myModalLabel">Computer Science Society</h4>
+          <h4 class="modal-title" id="event-title">Event Information</h4>
         </div>
         <div class="modal-body">
-          <table class="table table-bordered table-striped">
-            <tbody>
-              <tr>
-                <td>Computer Science Society</td>
-              </tr>
-              <tr>
-                <td>
+          <div class="panel-group" id="accordion_1" role="tablist" aria-multiselectable="true">
+            <div class="panel panel-primary">
+              <div class="panel-heading" role="tab" id="headingOne_1">
+                <h4 class="panel-title">
+                  <a role="button" data-toggle="collapse" data-parent="#accordion_1" href="#collapseOne_1" aria-expanded="false" aria-controls="collapseOne_1" class="collapsed">
+                    Kadayawan sa UP
+                  </a>
+                </h4>
+              </div>
+              <div id="collapseOne_1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne_1" aria-expanded="false" style="height: 0px;">
+                <div class="panel-body">
+                  <p>Venue: University Gymnasium</p>
                   <p>
-                    First appeared in 2017, when the two of the student of Bachelor of Science in Computer Science
-                    Namely Larry Page and Sergey Brin in time of on going research conventional search engine that
-                    ranked results by counting how many times the search terms appeared on the page and
-                    analyzed the relationships among websites called pagerank
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute,
+                    non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum
+                    eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid
+                    single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh
+                    helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+                    Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table,
+                    raw denim aesthetic synth nesciunt you probably haven't heard of them
+                    accusamus labore sustainable VHS.
                   </p>
-                  <p>These project grow and now what is known as google</p>
-                </td>
-              </tr>
-              <tr>
-                <td>Lead by: Jeff Bezos</td>
-              </tr>
-              <tr>
-                <td>Aniversary: September 25, 2017</td>
-              </tr>
-            </tbody>
-          </table>
+                </div>
+              </div>
+            </div>
+            <div class="panel panel-primary">
+              <div class="panel-heading" role="tab" id="headingTwo_1">
+                <h4 class="panel-title">
+                  <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion_1" href="#collapseTwo_1" aria-expanded="false" aria-controls="collapseTwo_1">
+                    Configure Social Notification
+                  </a>
+                </h4>
+              </div>
+              <div id="collapseTwo_1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo_1" aria-expanded="false">
+                <div class="panel-body">
+                  <table class="table table-bordered table-striped">
+                    <thead>
+                      <th>Advertising Options</th>
+                      <th colspan="2">Reminders</th>
+                      <th>Audience</th>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>
+                          <div class="demo-switch">
+                            <div class="switch" id="facebook">
+                              <label>OFF<input type="checkbox" name="facebook" checked><span class="lever switch-col-indigo"></span>ON</label> Facebook
+                            </div>
+                          </div>
+                        </td>
+                        <td>
+                          <select class="form-control show-tick" name="">
+                            <option value="">1</option>
+                            <option value="">2</option>
+                            <option value="">3</option>
+                            <option value="">4</option>
+                          </select>
+                        </td>
+                        <td>
+                          <select class="form-control show-tick" name="">
+                            <option value="">day</option>
+                            <option value="">week</option>
+                            <option value="">month</option>
+                            <option value="">year</option>
+                          </select>
+                        </td>
+                        <td>
+                          <select class="form-control show-tick" name="">
+                            <option value="">University</option>
+                            <option value="">Organization</option>
+                          </select>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <div class="demo-switch">
+                            <div class="switch" id="twitter">
+                              <label>OFF<input type="checkbox" name="twitter" checked><span class="lever switch-col-indigo"></span>ON</label> Twitter
+                            </div>
+                          </div>
+                        </td>
+                        <td rowspan="4" colspan="3">Occuppied</td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <div class="demo-switch">
+                            <div class="switch" id="email">
+                              <label>OFF<input type="checkbox" name="email" checked><span class="lever switch-col-indigo"></span>ON</label> Email
+                            </div>
+                          </div>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <div class="demo-switch">
+                            <div class="switch" id="mobile">
+                              <label>OFF<input type="checkbox" name="mobile" checked><span class="lever switch-col-indigo"></span>ON</label> Mobile
+                            </div>
+                          </div>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <div class="modal-footer">
-          <i class="material-icons" data-dismiss="modal" style="cursor:pointer;">close</i>
+          <button type="button" data-color="red" class="btn bg-green waves-effect">Request Approval</button>
+          <button type="button" class="btn btn-link waves-effect" data-dismiss="modal">CLOSE</button>
         </div>
       </div>
     </div>
@@ -165,5 +247,6 @@
   <script src="{{ asset('js/bootstrap-select.js') }}"?v=0.1></script>
   <script src="{{ asset('js/jquery.dataTables.js') }}"?v=0.1></script>
   <script src="{{ asset('js/jquery-datatable.js') }}"?v=0.1></script>
+  <script src="{{ asset('js/bootstrap-select.js') }}"?v=0.1></script>
 </body>
 </html>
