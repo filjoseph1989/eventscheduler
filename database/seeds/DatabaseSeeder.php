a3a9e6997e2bab9eebb2d23949420f4fe82b9cac
jlvoice777 <fil@greyandgreentech.com>
2017-09-28 09:00:58 +0800

    Make improvements on seeder informations

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,14 +14,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
       $this->call(CoursesTableSeeder::class);
-      $this->call(DepartmentsTableSeeder::class);
-      $this->call(EventCategoriesTableSeeder::class);
       $this->call(EventTypesTableSeeder::class);
+      $this->call(SemestersTableSeeder::class);
       $this->call(OrganizationsTableSeeder::class);
       $this->call(PositionsTableSeeder::class);
-      $this->call(SAdminsTableSeeder::class);
-      $this->call(UserAccountsTableSeeder::class);
-      $this->call(UsersTableSeeder::class);
       $this->call(EventsTableSeeder::class);
+      // $this->call(UsersTableSeeder::class);
     }
 }
