3310bb563c395ebde0a2a414998f9d1ff2aaa393
jlvoice777 <fil@greyandgreentech.com>
2017-09-28 09:01:38 +0800

    Added new model event

---
+++ app/Models/Event.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Event extends Model
+{
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+    'event_type_id',
+    'semester_id',
+    'title',
+    'description',
+    'venue',
+    'date_start',
+    'date_end',
+    'date_start_time',
+    'date_end_time',
+    'whole_day',
+    'status',
+    'is_approve',
+    'twitter',
+    'twitter_msg',
+    'twitter_img',
+    'facebook',
+    'facebook_msg',
+    'facebook_img',
+    'sms',
+    'sms_msg',
+    'email',
+    'email_msg',
+    'email_img',
+  ];
+}
