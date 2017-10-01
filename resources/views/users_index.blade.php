881ff5274b952e9e4893316b4095e577f3329d21
jlvoice777 <fil@greyandgreentech.com>
2017-10-01 23:47:46 +0800

    Good bye Katherine Mcnamara Good night liz

--- resources/views/users_index.blade.php
+++ resources/views/users_index.blade.php
@@ -95,7 +95,7 @@
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
-          <h4 class="modal-title" id="full-name">Katherine Mcnamara</h4>
+          <h4 class="modal-title" id="full-name"></h4>
         </div>
         <div class="modal-body">
           <table class="table table-bordered table-striped">
