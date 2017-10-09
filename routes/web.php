6da1f1d99e8659e36cf1a713ea01265731a3be98
Liz N. De Guzman <janicalizdeguzman@gmail.com>
2017-10-09 23:12:55 +0800

    #initialized authentication

--- routes/web.php
+++ routes/web.php
@@ -16,36 +16,40 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+
 Route::name('my.login')->post('/my-login', 'Auth\LoginController@myLogin');
 
-Route::resource('home',         'HomeController');
-Route::resource('User',         'UserController');
-Route::resource('Org',          'OrganizationController');
-Route::resource('Calendar',     'CalendarController');
-Route::resource('Attendances',  'AttendanceController');
-Route::resource('Approve',      'ApproveEventController');
-Route::resource('Event',        'EventController', ['parameters' =>[
-  'Event' => 'id'
-]]);
-Route::resource('Attendances', 'AttendanceController', ['parameters' =>[
-  'Attendance' => 'id'
-]]);
-
-# Used for modal request
-Route::prefix('modals')->group(function() {
-  Route::name('modal.getUser')->post('/get/user','ModalController@getUser');
-  Route::name('modal.getCourse')->post('/get/course','ModalController@getCourse');
-  Route::name('modal.getPosition')->post('/get/position','ModalController@getPosition');
-  Route::name('modal.getOrganization')->post('/get/organization','ModalController@getOrganization');
-  Route::name('modal.getAttendance')->post('/get/attendance','ModalController@getAttendance'); 
+Route::group(['middleware'=>['auth']], function(){
+    Route::resource('home',         'HomeController');
+    Route::resource('User',         'UserController');
+    Route::resource('Org',          'OrganizationController');
+    Route::resource('Calendar',     'CalendarController');
+    Route::resource('Attendances',  'AttendanceController');
+    Route::resource('Approve',      'ApproveEventController');
+    Route::resource('Event',        'EventController', ['parameters' =>[
+      'Event' => 'id'
+    ]]);
+    Route::resource('Attendances', 'AttendanceController', ['parameters' =>[
+      'Attendance' => 'id'
+    ]]);
+    
+    # Used for modal request
+    Route::prefix('modals')->group(function() {
+      Route::name('modal.getUser')->post('/get/user','ModalController@getUser');
+      Route::name('modal.getCourse')->post('/get/course','ModalController@getCourse');
+      Route::name('modal.getPosition')->post('/get/position','ModalController@getPosition');
+      Route::name('modal.getOrganization')->post('/get/organization','ModalController@getOrganization');
+      Route::name('modal.getAttendance')->post('/get/attendance','ModalController@getAttendance'); 
+    });
+    
+    # Used for attendances
+    Route::prefix('attendance')->group(function() {
+      Route::name('attendance.official')->post('/get/official/attendance','AttendanceController@getOfficialAttendance');
+      Route::name('attendance.expected')->post('/get/expected/attendance','AttendanceController@getExpectedAttendance');
+      Route::name('attendance.confirmed')->post('/get/confirmed/attendance','AttendanceController@getConfirmedAttendance');
+      Route::name('attendance.declined')->post('/get/declined/attendance','AttendanceController@getDeclinedAttendance');
+      // Route::name('attendance.within')->post('/get/attendance','AttendanceController@getUserOrgs');  (<-edit)
+      //for later when authenitcation is complete(for within orgs of a user)
+    });
 });
 
-# Used for attendances
-Route::prefix('attendance')->group(function() {
-  Route::name('attendance.official')->post('/get/official/attendance','AttendanceController@getOfficialAttendance');
-  Route::name('attendance.expected')->post('/get/expected/attendance','AttendanceController@getExpectedAttendance');
-  Route::name('attendance.confirmed')->post('/get/confirmed/attendance','AttendanceController@getConfirmedAttendance');
-  Route::name('attendance.declined')->post('/get/declined/attendance','AttendanceController@getDeclinedAttendance');
-  // Route::name('attendance.within')->post('/get/attendance','AttendanceController@getUserOrgs');  (<-edit)
-  //for later when authenitcation is complete(for within orgs of a user)
-});
