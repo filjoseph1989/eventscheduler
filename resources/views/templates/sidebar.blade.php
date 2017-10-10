07b1a17d1007113bdbcf0b45c8edc2e7def0659b
Liz N. De Guzman <janicalizdeguzman@gmail.com>
2017-10-11 00:57:31 +0800

    # functional and can be seen at side-bar "Register New User" for organization-head-user if login, and will not appear is the user is not organization-head-user

--- resources/views/templates/sidebar.blade.php
+++ resources/views/templates/sidebar.blade.php
@@ -12,7 +12,7 @@
         <div class="email">{{ session('user_account') }}</div>
         <div class="btn-group user-helper-dropdown">
           <i class="material-icons" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">keyboard_arrow_down</i>
-          <ul class="dropdown-menu pull-right">
+          <ul class="dropdown-menu pull-right"> 
             <li><a href="{{ route('Profile.index') }}"><i class="material-icons">person</i>Profile</a></li>
             <li role="seperator" class="divider"></li>
             <li>
@@ -43,7 +43,9 @@
           </a>
           <ul class="ml-menu">
             <li><a href="{{ route('User.index') }}">List of Users</a></li>
-            <li><a href="{{ route('User.create') }}">Register New User</a></li>
+             @if (session('user_account') == 'organization-head-user')
+                <li><a href="{{ route('User.create') }}">Register New User</a></li>
+             @endif
           </ul>
         </li>
         <li>
