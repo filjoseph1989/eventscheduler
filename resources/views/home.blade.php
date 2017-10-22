71f47e542269b79f79e9e4f4d890ec3e851ef7fa
Liz N. De Guzman <janicalizdeguzman@gmail.com>
2017-10-22 23:55:16 +0800

    # updated home.blade.php for OSA

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -46,13 +46,19 @@
           <div class="card">
             <div class="header">
               <h2> MANAGE NOTIFICATIONS
-                <small>you panel for notification management</small>
+                @if (Auth::user()->user_type_id == 1)
+                  <small>you panel for notification management for your org's events or your personal events</small>
+                @else
+                  <small>you panel for notification management for your personal events</small>
+                @endif
               </h2>
             </div>
             <div class="body">
               <div class="list-group">
                 <a href="javascript:void(0);" class="list-group-item"> Edit Notification Settings </a>
+              @if (Auth::user()->user_type_id == 3)
                 <a href="javascript:void(0);" class="list-group-item"> Approve Events </a>
+              @endif
               </div>
             </div>
           </div>
