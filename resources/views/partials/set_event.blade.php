bdc52939405f637ad90ea91334c815cb79364325
Liz N. De Guzman <janicalizdeguzman@gmail.com>
2017-10-30 22:19:56 +0800

    # made changes at Osa's partials for home blade

--- resources/views/partials/set_event.blade.php
+++ resources/views/partials/set_event.blade.php
@@ -2,11 +2,12 @@
   <div class="header">
     @if (Auth::user()->user_type_id == 3)
       <h2> ADVERTISE / SET EVENT
-        <small>In this panel you set event/s or approve event/s for advertisement</small>
+        <small>In this panel you approve event/s for advertisement</small>
+        <small>In this panel you view or set your personal event/s or events for your office</small>        
       </h2>
     @elseif(Auth::user()->user_type_id == 1)
       <h2> SET EVENT
-        <small>In this panel you set your personal event/s or events within your organization</small>
+        <small>In this panel you view or set your personal event/s or events for your organization</small>
       </h2>
     @else
       <h2>
@@ -19,8 +20,11 @@
       {{-- sa side-bar na lang ang create events, dri kay mag check na lang jud sa list of events tapos
         approve.. kulang pa ata ang list of event og is_approve status tapos kailangan pud makita iyang type of official
         event, kung university or organizations sa sulod na lang sa link sa event tung status na field --}}
-      <a href="{{ route('Event.index') }}" class="list-group-item"> Official Events </a>
-      <a href="{{ route('Event.show', 2) }}" class="list-group-item"> Personal Events </a>
+      @if (Auth::user()->user_type_id == 3)
+        <a href="{{ route('Event.index') }}" class="list-group-item"> Approve Official Events </a>
+      @endif
+        <a href="{{ route('Event.show', 1) }}" class="list-group-item"> Official Events </a>      
+        <a href="{{ route('event.dlv', 2) }}"  class="list-group-item" > Personal Events </a>
     </div>
   </div>
 </div>
