83dabb951f735835d7b77f6201f81aed9d9a2e37
jlvoice777 <fil@greyandgreentech.com>
2017-11-05 01:18:28 +0800

    By running php artisan send:notifications. In the background, it will sent reminders about the upcoming event to those members who confirmed attendance

--- app/Console/Commands/SendNotifications.php
+++ app/Console/Commands/SendNotifications.php
@@ -5,10 +5,12 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 
 use App\Models\Event;
+use App\Models\Attendance;
+use App\Models\OrganizationGroup;
 
 /**
  * Send event reminder in the background
- * 
+ *
  * @author Janica Liz <janicalizdeguzman@gmail.com>
  * @version 1.0.0
  * @date 11-05-2017
@@ -31,6 +33,12 @@ class SendNotifications extends Command
     protected $description = 'This command will sent a reminder of the event to organization members';
 
     /**
+     * email handle
+     * @var array
+     */
+    private $emails    = [];
+
+    /**
      * Create a new command instance.
      *
      * @return void
@@ -47,7 +55,17 @@ class SendNotifications extends Command
      */
     public function handle()
     {
-        var_dump(self::getEvents);
+        foreach (self::getEvents() as $key => $event) {
+          self::getUser($event);
+
+          $month = date('m', strtotime($event->date_start));
+          $day   = date('d', strtotime($event->date_start)) - date('d');
+
+          # Send an email reminder to user
+          if ($month == date('m') AND ($day == 1 OR $day == 2 OR $day == 3)) {
+            self::sendEmail($event);
+          }
+        }
     }
 
     /**
@@ -58,12 +76,65 @@ class SendNotifications extends Command
     private function getEvents()
     {
         return Event::with('organization')
-            ->where('category', 'organizations')
-            ->orWhere('category', 'within')
+            ->where(function($query) {
+                $query
+                  ->where('category', 'organization')
+                  ->orWhere('category', 'within');
+            })
             ->where('status', 'upcoming')
             ->where('is_approve', 'true')
             ->whereYear("date_start", '=', date('Y'))
             ->whereMonth('date_start','>=',date('m'))
             ->get();
+    }
+
+    /**
+     * collect the list of user in an event
+     *
+     * @param  object $event
+     * @return
+     */
+    private function getUser($event)
+    {
+      $organizationGroup = OrganizationGroup::with('user')
+        ->where('organization_id', $event->organization->id)
+        ->get();
+
+      $attendance = Attendance::with('user')
+        ->where('event_id', $event->id)
+        ->where('status', 'confirmed')
+        ->get();
+
+      self::gatheringEmails($organizationGroup);
+      self::gatheringEmails($attendance);
+    }
+
+    /**
+     * Gather user emails
+     *
+     * @param  object $data
+     * @return object
+     */
+    private function gatheringEmails($data)
+    {
+      foreach ($data as $key => $value) {
+        if (! isset($this->emails[$value->user->id])) {
+          $this->emails[$value->user->id] = $value->user->email;
+        }
+      }
+    }
+
+    /**
+     * Send emails
+     *
+     * @param  object $event
+     * @return void
+     */
+    private function sendEmail($event)
+    {
+      foreach($this->emails as $key => $email) {
+        Mail::to($email)
+          ->send(new ApproveEmailNotification($event));
+      }
     }
 }
